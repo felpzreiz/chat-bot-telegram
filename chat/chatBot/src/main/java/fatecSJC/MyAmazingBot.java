0b467c3538d957bf6ba7e926afb87e252607bfca
Felipe Reis <93730786+felpzreiz@users.noreply.github.com>
2024-08-27 22:39:04 -0300

    - Saudações e Detecção de Palavras
    - adição de variável com operador ternário para identificar o horário e a saudação correspondente;
    
    - adição de código para destrinchar a mensagem do usuário e responder especificamente.

--- chat/chatBot/src/main/java/fatecSJC/MyAmazingBot.java
+++ chat/chatBot/src/main/java/fatecSJC/MyAmazingBot.java
@@ -7,6 +7,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 public class MyAmazingBot implements LongPollingSingleThreadUpdateConsumer {
@@ -16,6 +19,14 @@ public class MyAmazingBot implements LongPollingSingleThreadUpdateConsumer {
         telegramClient = new OkHttpTelegramClient(botToken);
     }
 
+    Date Tempo = new Date();
+
+    String saudacao = ((Tempo.getHours() >= 18)
+            ?"Boa noite!"
+            :(Tempo.getHours() >= 12)
+                ?"Boa tarde!"
+                :"Bom dia!");
+
     @Override
     public void consume(Update update) {
         // We check if the update has a message and the message has text
@@ -24,11 +35,12 @@ public class MyAmazingBot implements LongPollingSingleThreadUpdateConsumer {
             String message_text = update.getMessage().getText();
 
             String[] words = message_text.split(" ");
+            List listaPalavras = Arrays.asList(words);
 
             if (Objects.equals(message_text, "/start")){
-                message_text = "Olá, eu sou o Ajudante de Livraria :books: \nComo posso te ajudar?";
-            } else if (Objects.equals(message_text, "Livro")){
-
+                message_text = saudacao + " Eu sou o Ajudante de Livraria! \nComo posso te ajudar?";
+            }else if (listaPalavras.contains("piada")){
+                message_text = "hahaha";
             }else{
                 message_text = "Não compreendi o que você quis dizer.\nPoderia enviar outra mensagem?";
             }
